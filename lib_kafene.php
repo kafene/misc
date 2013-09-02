6308acdb2b88a091b01d3f6c5605d56c93862986
kafene <kafene@users.noreply.github.com>
2013-09-02 19:08:03 -0400

    Update lib_kafene.php

--- lib_kafene.php
+++ lib_kafene.php
@@ -468,3 +468,66 @@ function handlebar($path, $vars = []) {
 }
 
 
+# MicroTpl
+# http://github.com/unu/microtpl
+class MicroTpl {
+    static function parse($tpl) {
+        return preg_replace([
+            '_{\@([^}]+)}_', # {@list} list array
+            '_{\?([^}]+)}_', # {?bool} show on true
+            '_{\!([^}]+)}_', # {!bool} show on false
+            '_{\/([^}]+)}_', # {/list} closing mark
+            '_{\&([^}]+)}_', # {&var} unescaped echo
+            '_{([a-zA-Z0-9]+)}_', # {var} escaped echo
+            '_{([a-zA-Z0-9]+=[^}]+)}_', # {var='value'} assign variable
+            '_{-?([^ }][^}]*)}_', # {php code} php code
+        ], [
+            '<?php $_save_\1=get_defined_vars();'
+            . 'foreach((isset($\1)&&is_array($\1)?$\1:array())as$_item){ '
+            . 'if(is_array($_item))extract($_item)?>',
+            '<?php if(isset($\1)&&!!$\1){ ?>',
+            '<?php if(!isset($\1)||!$\1){ ?>',
+            '<?php }if(isset($_save_\1)&&is_array($_save_\1))extract($_save_\1)?>',
+            '<?php echo isset($\1)?$\1:null?>',
+            '<?php echo isset($\1)?htmlspecialchars(\$\1,ENT_QUOTES):null?>',
+            '<?php $this->\1?>',
+            '<?php \1?>',
+        ], $tpl);
+    }
+    function render($_tpl, $_return = false) {
+        if ($_return) {
+            ob_start();
+        }
+        extract((array) $this);
+        eval('?>'.self::parse($_tpl));
+        if ($_return) {
+            return ob_get_clean();
+        }
+    }
+    function renderFile($file, $return = false) {
+        return $this->render(file_get_contents($file), $return);
+    }
+}
+/*
+$tpl = new MicroTpl();
+$tpl->title = 'MicroTpl';
+$tpl->messages = [
+    ['message' => 'Hello, Earth'],
+    ['message' => 'We confiscates this planet.'],
+];
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{title}</title>
+  </head>
+  <body>
+    <h1>{title}</h1>
+    {@messages}
+      <p>{message}</p>
+    {/messages}
+  </body>
+</html>
+*/
+
+
+
